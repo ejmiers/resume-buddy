b87c230d664c9b1229e88a7ed9549b0448983306
Eric Miers <eric.miers.15@cnu.edu>
2019-10-28 23:39:04 -0400

    added home, about, login, and signup routes

--- flask_server/routes.py
+++ flask_server/routes.py
@@ -4,4 +4,16 @@ from flask_server import app
 @app.route("/")
 @app.route("/home")
 def home():
-    return
+    return render_template('home.html', title='Home')
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+@app.route("/login")
+def login():
+    return render_template('login.html', title='Login')
+
+@app.route("/signup")
+def login():
+    return render_template('login.html', title='Login')
